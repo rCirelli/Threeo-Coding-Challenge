33021ab5361a1a43f17a9b39e1232204ccb8d07e
Raphael Cirelli <raphael.cirelli@protonmail.com>
2024-01-22 16:30:38 -0300

    FEAT: create Calculator service

---
+++ backend/src/services/CalculatorService.ts
@@ -0,0 +1,18 @@
+export default class Calculator {
+
+  public sum(num1: number, num2: number) : number {
+    return num1 + num2;
+  }
+
+  public subtract(num1: number, num2: number) : number {
+    return num1 - num2;
+  }
+
+  public multiply(num1: number, num2: number) : number {
+    return num1 * num2;
+  }
+
+  public divide(num1: number, num2: number) : number {
+    return num1 / num2;
+  }
+}
