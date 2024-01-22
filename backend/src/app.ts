73af0bc3aa07097cb61a793a72effd8cf9c7cbaa
Raphael Cirelli <raphael.cirelli@protonmail.com>
2024-01-22 16:29:55 -0300

    FEAT: create app and server files

---
+++ backend/src/app.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import * as router from './routes';
+
+class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+
+    this.config();
+    this.router();
+  }
+
+  private config():void {
+    this.app.use(express.json());
+  }
+
+  private router():void {
+    this.app.use('/calc', router.calculateRoute);
+  }
+
+  public start(PORT: string | number):void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+}
+
+export { App };
